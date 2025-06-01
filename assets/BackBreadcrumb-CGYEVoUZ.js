31344289b1e3b4eb39315dcaaa9a0c14da3e8778
mahadeer2008 <mahadeer2008@users.noreply.github.com>
2025-06-01 21:00:08 +0000

    deploy: e214ac0612f40e0e8f07a0853b46f1383c6a8b1d

---
+++ assets/BackBreadcrumb-CGYEVoUZ.js
@@ -0,0 +1,11 @@
+import{h as s,j as e,L as c}from"./index-Cz_5D_Uk.js";/**
+ * @license lucide-react v0.491.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=s("arrow-left",n);/**
+ * @license lucide-react v0.491.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],l=s("chevron-right",o),i=({href:t,label:a})=>e.jsx("div",{className:"w-full pt-8",children:e.jsxs(c,{to:t,className:"inline-flex items-center text-sm text-gray-500 hover:text-gray-700 transition",children:[e.jsx(r,{className:"w-4 h-4 mr-1 text-black"}),e.jsx("span",{className:"font-medium",children:"Home"}),e.jsx("span",{className:"mx-1",children:e.jsx(l,{className:"w-4 h-4 text-black"})}),e.jsx("span",{className:"text-gray-700",children:a})]})});export{i as B};
